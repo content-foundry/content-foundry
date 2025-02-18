b4acfdd951e0f5fad2267046ed9b79a9c8ad08b7
runner@4034b9026e4e <>
2025-02-18 15:10:21 +0000

    Update bff pull
    Summary:
    
    Test Plan:

--- infra/bff/friends/pull.bff.ts
+++ infra/bff/friends/pull.bff.ts
@@ -1,3 +1,4 @@
+
 import { register } from "infra/bff/bff.ts";
 import { runShellCommand } from "infra/bff/shellBase.ts";
 import { getLogger } from "packages/logger.ts";
@@ -6,9 +7,30 @@ const logger = getLogger(import.meta);
 
 export async function pull(): Promise<number> {
   logger.info("Pulling latest code from sapling...");
-  return await runShellCommand([
+  const pullResult = await runShellCommand([
     "sl",
     "pull",
+  ]);
+
+  if (pullResult !== 0) {
+    return pullResult;
+  }
+
+  logger.info("Going to remote/main...");
+  const gotoResult = await runShellCommand([
+    "sl",
+    "goto",
+    "remote/main",
+  ]);
+
+  if (gotoResult !== 0) {
+    return gotoResult;
+  }
+
+  logger.info("Installing dependencies...");
+  return await runShellCommand([
+    "deno",
+    "install",
   ]);
 }
 
