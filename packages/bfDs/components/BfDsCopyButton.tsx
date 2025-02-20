e8b9fa85ed60367c029851f3715e178cabe6c44f
Justin Carter <justin@boltfoundry.com>
2025-02-20 00:08:15 +0000

    update bfds things
    Summary:
    
    Test Plan:

--- packages/bfDs/components/BfDsCopyButton.tsx
+++ packages/bfDs/components/BfDsCopyButton.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { BfDsButton } from "packages/bfDs/components/BfDsButton.tsx";
+import {
+  BfDsButton,
+  type ButtonKind,
+} from "packages/bfDs/components/BfDsButton.tsx";
 import { useCopyToClipboard } from "packages/bfDs/hooks/useCopyToClipboard.ts";
 type Props = {
   textToCopy: string;
   buttonText?: string;
+  kind?: ButtonKind;
 };
 const { useState } = React;
-export function BfDsCopyButton({ textToCopy, buttonText = "Copy" }: Props) {
+export function BfDsCopyButton(
+  { textToCopy, buttonText = "Copy", kind = "overlay" }: Props,
+) {
   const [copied, setCopied] = useState(false);
   const [, copy] = useCopyToClipboard();
   const handleCopy = () => {
@@ -17,5 +23,5 @@ export function BfDsCopyButton({ textToCopy, buttonText = "Copy" }: Props) {
 
   const text = copied ? "Copied!" : buttonText;
 
-  return <BfDsButton kind="overlay" text={text} onClick={handleCopy} />;
+  return <BfDsButton kind={kind} text={text} onClick={handleCopy} />;
 }
