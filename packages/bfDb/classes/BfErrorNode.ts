302b4b0964489497b36f3847561b0fc0d247871c
George LeVitre <george@boltfoundry.com>
2025-02-21 04:15:52 +0000

    Better error message
    Summary:
    
    Test Plan:

--- packages/bfDb/classes/BfErrorNode.ts
+++ packages/bfDb/classes/BfErrorNode.ts
@@ -2,4 +2,8 @@ import { BfError } from "packages/BfError.ts";
 
 export class BfErrorNode extends BfError {}
 
-export class BfErrorNodeNotFound extends BfErrorNode {}
+export class BfErrorNodeNotFound extends BfErrorNode {
+  constructor(message = "Node not found") {
+    super(message);
+  }
+}
