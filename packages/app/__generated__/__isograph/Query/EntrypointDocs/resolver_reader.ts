ae3a51ba42be3734aec53e5c8b873b8d96090fc1
Randall Bennett <{"message":"Not Found","documentation_url":"https://docs.github.com/rest/users/emails#list-email-addresses-for-the-authenticated-user","status":"404"}>
2025-02-21 16:51:23 +0000

    commit for randallb
    Summary:
    
    Test Plan:

--- packages/app/__generated__/__isograph/Query/EntrypointDocs/resolver_reader.ts
+++ packages/app/__generated__/__isograph/Query/EntrypointDocs/resolver_reader.ts
@@ -2,33 +2,13 @@ import type { EagerReaderArtifact, ReaderAst } from '@isograph/react';
 import { Query__EntrypointDocs__param } from './param_type.ts';
 import { Query__EntrypointDocs__output_type } from './output_type.ts';
 import { EntrypointDocs as resolver } from '../../../../entrypoints/EntrypointDocs.tsx';
-import BfDocs__DocsList__resolver_reader from '../../BfDocs/DocsList/resolver_reader.ts';
 
 const readerAst: ReaderAst<Query__EntrypointDocs__param> = [
   {
-    kind: "Linked",
-    fieldName: "me",
+    kind: "Scalar",
+    fieldName: "__typename",
     alias: null,
     arguments: null,
-    condition: null,
-    selections: [
-      {
-        kind: "Linked",
-        fieldName: "docs",
-        alias: null,
-        arguments: null,
-        condition: null,
-        selections: [
-          {
-            kind: "Resolver",
-            alias: "DocsList",
-            arguments: null,
-            readerArtifact: BfDocs__DocsList__resolver_reader,
-            usedRefetchQueries: [],
-          },
-        ],
-      },
-    ],
   },
 ];
 
