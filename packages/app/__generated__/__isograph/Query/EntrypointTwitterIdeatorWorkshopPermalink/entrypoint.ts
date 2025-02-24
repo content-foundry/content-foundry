90c0390b20ab4721ec6d74419df916da9253eb83
runner@4a01cbbe8c99 <>
2025-02-24 15:45:52 +0000

    [2/2] Use codegen for routes
    Summary:
    
    Test Plan:

---
+++ packages/app/__generated__/__isograph/Query/EntrypointTwitterIdeatorWorkshopPermalink/entrypoint.ts
@@ -0,0 +1,75 @@
+import type {IsographEntrypoint, NormalizationAst, RefetchQueryNormalizationArtifactWrapper} from '@isograph/react';
+import {Query__EntrypointTwitterIdeatorWorkshopPermalink__param} from './param_type.ts';
+import {Query__EntrypointTwitterIdeatorWorkshopPermalink__output_type} from './output_type.ts';
+import readerResolver from './resolver_reader.ts';
+const nestedRefetchQueries: RefetchQueryNormalizationArtifactWrapper[] = [];
+
+const queryText = 'query EntrypointTwitterIdeatorWorkshopPermalink  {\
+  me {\
+    __typename,\
+    id,\
+    ... on BfCurrentViewerLoggedIn {\
+      id,\
+      __typename,\
+    },\
+  },\
+}';
+
+const normalizationAst: NormalizationAst = {
+  kind: "NormalizationAst",
+  selections: [
+    {
+      kind: "Linked",
+      fieldName: "me",
+      arguments: null,
+      concreteType: null,
+      selections: [
+        {
+          kind: "Scalar",
+          fieldName: "__typename",
+          arguments: null,
+        },
+        {
+          kind: "Scalar",
+          fieldName: "id",
+          arguments: null,
+        },
+        {
+          kind: "InlineFragment",
+          type: "BfCurrentViewerLoggedIn",
+          selections: [
+            {
+              kind: "Scalar",
+              fieldName: "id",
+              arguments: null,
+            },
+            {
+              kind: "Scalar",
+              fieldName: "__typename",
+              arguments: null,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+const artifact: IsographEntrypoint<
+  Query__EntrypointTwitterIdeatorWorkshopPermalink__param,
+  Query__EntrypointTwitterIdeatorWorkshopPermalink__output_type
+> = {
+  kind: "Entrypoint",
+  networkRequestInfo: {
+    kind: "NetworkRequestInfo",
+    queryText,
+    normalizationAst,
+  },
+  concreteType: "Query",
+  readerWithRefetchQueries: {
+    kind: "ReaderWithRefetchQueries",
+    nestedRefetchQueries,
+    readerArtifact: readerResolver,
+  },
+};
+
+export default artifact;
---
+++ packages/app/__generated__/__isograph/Query/entrypointTwitterIdeatorResearchPermalink/output_type.ts
@@ -0,0 +1,3 @@
+import type React from 'react';
+import { entrypointTwitterIdeatorResearchPermalink as resolver } from '../../../../entrypoints/EntrypointTwitterIdeatorResearchPermalink.ts';
+export type Query__entrypointTwitterIdeatorResearchPermalink__output_type = ReturnType<typeof resolver>;
