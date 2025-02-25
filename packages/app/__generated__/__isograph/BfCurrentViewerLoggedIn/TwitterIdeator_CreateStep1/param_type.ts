a4d3fd0407591b8ccddb060383b3084e8f869ff0
runner@9d6d39659e5e <>
2025-02-25 22:22:43 +0000

    remove old twitter scaffolding
    Summary:
    
    Test Plan:

--- packages/app/__generated__/__isograph/BfCurrentViewerLoggedIn/TwitterIdeator_CreateStep1/param_type.ts
+++
@@ -1,7 +0,0 @@
-
-export type BfCurrentViewerLoggedIn__TwitterIdeator_CreateStep1__param = {
-  readonly data: {
-    readonly __typename: string,
-  },
-  readonly parameters: Record<PropertyKey, never>,
-};
