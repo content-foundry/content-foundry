ddbd19b3b81992a4761dc633465e97ac5d5919ea
runner@5fc3cac20488 <>
2025-02-25 14:22:41 +0000

    rm docs route
    Summary:
    
    Test Plan:

--- packages/app/components/BfCurrentViewer/Docs.tsx
+++
@@ -1,9 +0,0 @@
-import { iso } from "packages/app/__generated__/__isograph/iso.ts";
-
-export const Docs = iso(`
-  field BfCurrentViewer.Docs @component {
-    __typename
-  }
-`)(function Docs() {
-  return <div>Documentation coming soon!</div>;
-});
