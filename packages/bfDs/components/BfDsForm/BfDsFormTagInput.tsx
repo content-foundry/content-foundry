44e3916bd745e031185bcf1b41f40e81c0f05a1b
runner@16062440a627 <>
2025-02-26 16:55:06 +0000

    add types to button and inputs
    Summary:
    
    Test Plan:

--- packages/bfDs/components/BfDsForm/BfDsFormTagInput.tsx
+++ packages/bfDs/components/BfDsForm/BfDsFormTagInput.tsx
@@ -1,18 +1,24 @@
 import { useBfDsFormContext } from "packages/bfDs/components/BfDsForm/BfDsForm.tsx";
-import { BfDsTagInput } from "packages/bfDs/components/BfDsTagInput.tsx";
+import {
+  BfDsTagInput,
+  type TagInputProps,
+} from "packages/bfDs/components/BfDsTagInput.tsx";
+
+type Props = { id: string; title: string } & TagInputProps;
 
 export function BfDsFormTagInput(
-  { id, title }: { id: string; title: string },
+  { id, title, ...props }: Props,
 ) {
   const { data, onChange } = useBfDsFormContext();
   if (!data) return null;
   return (
     <BfDsTagInput
-      label={title}
-      name={id}
+      {...props}
       // @ts-ignore: TODO @george, need to figure out typing
       value={data[id] ?? []}
       onChange={(tags) => onChange?.({ ...data, [id]: tags })}
+      label={title}
+      name={id}
     />
   );
 }
--- packages/bfDs/components/BfDsForm/BfDsFormToggle.tsx
+++ packages/bfDs/components/BfDsForm/BfDsFormToggle.tsx
@@ -2,16 +2,19 @@ import {
   type BfDsFormElementProps,
   useBfDsFormContext,
 } from "packages/bfDs/components/BfDsForm/BfDsForm.tsx";
-import { BfDsToggle } from "packages/bfDs/components/BfDsToggle.tsx";
+import {
+  BfDsToggle,
+  type ToggleBaseProps,
+} from "packages/bfDs/components/BfDsToggle.tsx";
 
-// TODO: Implement all props:
-// disabled, label, value, onChange, className, meta, name, required, readonly, size, testId
-
-export function BfDsFormToggle({ id, title }: BfDsFormElementProps) {
+export function BfDsFormToggle(
+  { id, title, ...props }: BfDsFormElementProps & ToggleBaseProps,
+) {
   const { data, onChange } = useBfDsFormContext();
   if (!data) return null;
   return (
     <BfDsToggle
+      {...props}
       label={title}
       name={id}
       // @ts-ignore: TODO @george, need to figure out typing
