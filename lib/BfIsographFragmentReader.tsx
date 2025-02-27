bd189ab2dc76e057fb74a6129aae0383500cc7da
runner@50e0fb4642ba <>
2025-02-27 16:12:53 +0000

    Update fragment reader to use new bfisographentrypoint type
    Summary:
    
    Test Plan:

--- lib/BfIsographFragmentReader.tsx
+++ lib/BfIsographFragmentReader.tsx
@@ -1,11 +1,10 @@
 import type * as React from "react";
-import type { ExtractReadFromStore, IsographEntrypoint } from "@isograph/react";
-import type { FragmentReference } from "@isograph/react";
-// import { NetworkRequestReaderOptions } from '@isograph/react';
+import type { ExtractReadFromStore, FragmentReference } from "@isograph/react";
 import { useResult } from "@isograph/react";
 import { getLogger } from "packages/logger.ts";
 import { BfError } from "packages/BfError.ts";
 import type { RouteEntrypoint } from "packages/app/__generated__/builtRoutes.ts";
+import type { BfIsographEntrypoint } from "lib/BfIsographEntrypoint.ts";
 
 const _logger = getLogger(import.meta);
 
@@ -15,8 +14,7 @@ type NetworkRequestReaderOptions = {
 };
 
 export function BfIsographFragmentReader<
-  // deno-lint-ignore no-explicit-any
-  TEntrypoint extends IsographEntrypoint<any, RouteEntrypoint>,
+  TEntrypoint extends BfIsographEntrypoint,
 >(
   props: {
     fragmentReference: FragmentReference<
