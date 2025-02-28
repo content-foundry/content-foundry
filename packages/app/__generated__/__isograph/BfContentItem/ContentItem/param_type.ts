b9ff58a895890005a03aefc6730e81fa6ddec07e
runner@5945075fecf7 <>
2025-02-28 16:35:59 +0000

    Create content item component
    Summary:
    
    Test Plan:

---
+++ packages/app/__generated__/__isograph/BfContentItem/ContentItem/param_type.ts
@@ -0,0 +1,10 @@
+
+export type BfContentItem__ContentItem__param = {
+  readonly data: {
+    readonly __typename: string,
+    readonly title: (string | null),
+    readonly body: (string | null),
+    readonly href: (string | null),
+  },
+  readonly parameters: Record<PropertyKey, never>,
+};
